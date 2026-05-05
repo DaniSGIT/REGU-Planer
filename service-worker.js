74e71c3deef387b0cbcab3b2dbfd7e2276dc10fa
DaniSGIT <95162660+DaniSGIT@users.noreply.github.com>
2026-05-05 18:42:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,25 @@
-self.addEventListener("install", () => {
+const APP_CACHE_VERSION = "regu-planer-1.9.2";
+
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== APP_CACHE_VERSION)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" }).catch(() => caches.match(event.request))
+  );
+});
